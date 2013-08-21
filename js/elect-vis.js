395a2f502a2459b87a81c6b0d0b461ffec2763f6
kennib <kenni.bawden@gmail.com>
2013-08-21 17:17:07 +1000

    Removed years from URIs and links

--- js/elect-vis.js
+++ js/elect-vis.js
@@ -4,9 +4,9 @@ electvis = angular.module('elect-vis', ['elect-vis-data']);
 electvis.config(function($routeProvider) {
 	$routeProvider.
 		when('', {controller: pageCtrl, templateUrl: 'pages/home.html'}).
-		when('/:year/electorate/:electorate', {controller: electorateCtrl, templateUrl: 'pages/electorate.html'}).
-		when('/:year/electorates', {controller: electorateCtrl, templateUrl: 'pages/electorates.html'}).
-		when('/:year/electorate', {redirectTo: '/:year/electorates'})
+		when('/electorate/:electorate', {controller: electorateCtrl, templateUrl: 'pages/electorate.html'}).
+		when('/electorates', {controller: electorateCtrl, templateUrl: 'pages/electorates.html'}).
+		when('/electorate', {redirectTo: '/:year/electorates'})
 });
 
 /* Default static-ish page controller */
@@ -16,8 +16,6 @@ function pageCtrl($scope) {
 /* Electorates data and visualisation controller */
 function electorateCtrl($scope, $routeParams,
                         electorates) {
-	$scope.year = $routeParams.year;
-
 	$scope.electorates = electorates;
 	
 	var electorateName = $routeParams.electorate;
