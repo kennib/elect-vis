a4f5144d9ed871cb7bb9e64e0daa7a3cbafd02c3
Kenni Bawden <kenni.bawden@nicta.com.au>
2013-09-06 10:57:49 +1000

    Added data service for github json files

--- js/elect-vis-data.js
+++ js/elect-vis-data.js
@@ -1,5 +1,29 @@
 electvisdata = angular.module('elect-vis-data', []);
 
+electvisdata.factory('githubData', function($http) {
+  var githubUrl = 'http://rawgithub.com/kennib/elect-data/master';
+  delete $http.defaults.headers.common['X-Requested-With'];
+
+  return function(file) {
+    return $http.get(githubUrl+'/'+file+'.json').then(function(response) {
+      return response.data;
+    });
+  };
+});
+
+electvisdata.factory('yearData', function(githubData) {
+  return function (year) {
+    var data = {};
+    var datasets = ['candidates', 'twocandidate', 'firstpreferences', 'electorates'];
+    angular.forEach(datasets, function(set) {
+      data[set] = githubData('data/'+year+'/'+set);
+    });
+
+    return data; 
+  }
+});
+
+
 electvisdata.factory('news', function($http) {
 	return $http.get('data/news.json').then(function(response) {
 		return response.data;
